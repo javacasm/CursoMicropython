1121cfdd27dbe2e9da68f65fa59ac20dd477adb0
javacasm <javacasm@gmail.com>
2022-01-24 21:09:44 +0100

    update

---
+++ TODO_video.md
@@ -0,0 +1,2 @@
+Simulador https://micropython.org/unicorn/
+
--- codigo/P.MedidorCO2/main.py
+++ codigo/P.MedidorCO2/main.py
@@ -2,19 +2,31 @@ import machine
 import ubinascii
 import medidaCO2
 
-v = '0.4'
+v = '0.4.2'
 
-msg = medidaCO2.initWifi('RED_WIFI','CLAVE_WIFI')
-print(f'IP:{msg}')
+mqtt_client = None
 
-client_id = ubinascii.hexlify(machine.unique_id())
+def connnectMQTT(p):
+    global mqtt_client
+    if mqtt_client == None:
+        msg = medidaCO2.initWifi('OpenWrt','qazxcvbgtrewsdf')
+        print(f'IP:{msg}')
 
-mqtt_server = 'io.adafruit.com'
-mqtt_port =  1883
+        client_id = ubinascii.hexlify(machine.unique_id())
 
-print(f'Conectando a {mqtt_server}@{mqtt_port}')
-clientMQTT = medidaCO2.connect_and_subscribe(client_id, mqtt_server, mqtt_port, 'USUARIO_ADAFRUIT','KEY_ADAFRUIT')
+        mqtt_server = 'io.adafruit.com'
+        mqtt_port =  1883
 
+        print(f'Conectando a {mqtt_server}@{mqtt_port}')
+        mqtt_client = medidaCO2.connect_and_subscribe(client_id, mqtt_server, mqtt_port, 'javacasm','aio_jCLm54QfTsjzr4s2ccoNoUokwyPt')
+    else:
+        print('Connected yet')
+        
+def pulsado(p):
+    print('Pulsado')
 
+# button = machine.Pin(23, machine.Pin.IN, machine.Pin.PULL_UP)
 
-medidaCO2.test_forever(clientMQTT)
+# button.irq(trigger = machine.Pin.IRQ_FALLING, handler = connnectMQTT)
+
+medidaCO2.test_forever(delay = 3)

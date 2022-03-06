6fc564f4400ffa775dc171aa4b15f1316bfead9a
javacasm <javacasm@gmail.com>
2022-03-06 23:28:53 +0100

    final Tema 7

--- codigo/mqtt_adafruit_io/config.py
+++ codigo/mqtt_adafruit_io/config.py
@@ -7,7 +7,7 @@ DEEP_SLEEP = True
 mqtt_server = 'io.adafruit.com'
 mqtt_port =  1883
 mqtt_user = 'USUARIO_IO'
-mqtt_password = 'aio_ADAFRUIT_IO
+mqtt_password = 'aio_ADAFRUIT_IO'
 BOARD = 'ESP32 - keyestudio'
 
 SSID = 'SSID_WIFI'

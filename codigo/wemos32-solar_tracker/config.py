28e923280315a5ca16a9a437f22b0e3950b7bc18
javacasm <javacasm@gmail.com>
2022-05-06 14:13:55 +0200

    update

--- codigo/wemos32-solar_tracker/config.py
+++ codigo/wemos32-solar_tracker/config.py
@@ -1,13 +1,14 @@
-v = '0.3'
+v = '0.4'
 
 pin_servoH = 27 # 6 Arduino
 pin_servoV = 16 # 5 Arduino
 
-pin_ldr00 = 35 # A2
+
+pin_ldr11 = 35 # A2
 pin_ldr01 = 34 # A3
 pin_ldr10 = 36 # A4
-pin_ldr11 = 39 # A5
+pin_ldr00 = 39 # A5
 
 
-pin_joyX = 35
-pin_joyY = 34
+pin_joyX = 02 # A0  Mejor 33 y 32 (extension de 3 pines)
+pin_joyY = 04 # A1
--- codigo/wemos32-solar_tracker/test_servo.py
+++ codigo/wemos32-solar_tracker/test_servo.py
@@ -5,38 +5,57 @@ import machine
 import time
 import config
 
-v = '0.6.2'
+v = '0.7.6'
 
-servoV = machine.PWM(machine.Pin(config.pin_servoV),freq = 50)
-servoH = machine.PWM(machine.Pin(config.pin_servoH),freq = 50)
-
-print(f'servoV @ {config.pin_servoV} ' + f'servoH @ {config.pin_servoH} ')
-print(f'ADCs: {config.pin_ldr00} {config.pin_ldr01} {config.pin_ldr10} {config.pin_ldr11} ')
+adc00 = adc01 = adc10 = adc11 = None
+servoH = servoV = None
+joyX = joyY = None
 
 minPos = 40
 maxPos = 115
 retardo = 5
 
-adc00 = machine.ADC(machine.Pin(config.pin_ldr00))
-adc01 = machine.ADC(machine.Pin(config.pin_ldr01))
-adc10 = machine.ADC(machine.Pin(config.pin_ldr10))
-adc11 = machine.ADC(machine.Pin(config.pin_ldr11))
 
-joyX = machine.ADC(machine.Pin(config.pin_joyX))
-joyX.width(machine.ADC.WIDTH_10BIT)
-joyX.atten(machine.ADC.ATTN_11DB)
+def initServos():
+    global servoV,servoH
+    servoV = machine.PWM(machine.Pin(config.pin_servoV),freq = 50)
+    servoH = machine.PWM(machine.Pin(config.pin_servoH),freq = 50)
+    print(f'servoV @ {config.pin_servoV} ' + f'servoH @ {config.pin_servoH} ')
+    print(f'ADCs: {config.pin_ldr00} {config.pin_ldr01} {config.pin_ldr10} {config.pin_ldr11} ')
 
-joyY = machine.ADC(machine.Pin(config.pin_joyY))
-joyY.width(machine.ADC.WIDTH_10BIT)
-joyY.atten(machine.ADC.ATTN_11DB)
+
+def setupADC(pin):
+    adc = machine.ADC(machine.Pin(pin))
+    adc.width(machine.ADC.WIDTH_10BIT)
+    adc.atten(machine.ADC.ATTN_11DB)
+    return adc
+
+def initADCs():
+    global adc00, adc01, adc10, adc11
+    adc00 = setupADC(config.pin_ldr00)
+    adc01 = setupADC(config.pin_ldr01)
+    adc10 = setupADC(config.pin_ldr10)
+    adc11 = setupADC(config.pin_ldr11)
+
+def initJoys():
+    global joyX, joyY
+    joyX = setupADC(config.pin_joyX)
+    joyY = setupADC(config.pin_joyY)
 
 def readJoys():
+    global joyX, joyY
+    if joyX == None:
+        initJoys()
     return joyX.read(), joyY.read()
 
 def readLDRs():
+    global adc00, adc01, adc10, adc11
+    if adc00 == None:
+        initADCs()
     return adc00.read(), adc01.read(), adc10.read(), adc11.read()
 
 def moveRangoServo(servo, inicial = minPos, final = maxPos, retardo = retardo):
+    global servoH, servoV
     paso = 1
     if final<inicial:
         paso = -1
@@ -53,11 +72,15 @@ def pos2grados(pos):
 def grados2pos(grados):
     return int((maxPos-minPos)*grados/180+minPos)
 
-def moveServos(gV,gH):
+def moveServos(gV, gH):
+    global servoH, servoV
+    if servoH == None:
+        initServos()
     servoH.duty(grados2pos(gH))
     servoV.duty(grados2pos(gV))
 
 def testRangos(minV = 0,maxV=180, minH = 0,maxH=180):
+    global servoH, servoV
     for h in range(minH,maxH,10):
         for v in range(minV, maxV,10):
             moveServos(v,h)
@@ -66,14 +89,30 @@ def testRangos(minV = 0,maxV=180, minH = 0,maxH=180):
         moveRangoServo(servoV,inicial=pos2grados(180),final = pos2grados(0))
 
 
-def controlServosWithJoy():
+def controlServosWithJoys_forever():
     while True:
-        jx,jy = readJoys()
-        gx = adc2grado(jx)
-        gy = adc2grado(jy)
+        controlServorsWithJoys()
+        
+def controlServosWithJoys(show = True):
+    jx,jy = readJoys()
+    gx = adc2grado(jx)
+    gy = adc2grado(jy)
+    if show:
         print(f'{jx:3d} > {gx:3d} {jy:3d} > {gy:3d} \r',end='')
-        moveServos(gy,gx)
+    moveServos(gy,gx)
 
-moveServos(90,90)
+def readLDRs_forever(pausa = 50):
+    while True:
+        print(f'{readLDRs()  }  ',end='\r')
+        time.sleep_ms(pausa)    
+
+
+def fullControl(pausa=50):
+    while True:
+        print(f'{readLDRs()}  ',end='\r')
+        controlServosWithJoys(show = False)
+        
+
+# moveServos(50,50)
 
 
